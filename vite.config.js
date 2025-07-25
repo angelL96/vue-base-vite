b1b70a966fe7b357bc6649692deb89bb54eb7af0
angelL96 <www.angel_luis96@hotmail.com>
2025-07-25 10:56:40 +0200

    added TypeScript

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    VueRouter(),
     vue(),
     vueDevTools(),
     Components({
@@ -31,7 +32,6 @@ export default defineConfig({
       },
       vueTemplate: true,
     }),
-    VueRouter(),
     Fonts(),
     Layouts(),
     tailwindcss(),
